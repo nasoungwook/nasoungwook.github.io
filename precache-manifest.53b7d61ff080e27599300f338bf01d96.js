626eeb4d108c6065a607d34549f64547ce889cc9
nasoungwook <wsn026@gmail.com>
2021-03-29 21:46:48 +0900

    Updates

---
+++ precache-manifest.53b7d61ff080e27599300f338bf01d96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fbf713163cd441e4074f58122bbfe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c927dab0852cf280aa4",
+    "url": "/static/css/main.f77d7a38.chunk.css"
+  },
+  {
+    "revision": "b5a2342bcbcb8a68fa8c",
+    "url": "/static/js/2.8635e6e3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.8635e6e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c927dab0852cf280aa4",
+    "url": "/static/js/main.011c8613.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/static/media/your photo.b9d7d448.png"
+  }
+]);
